master-hax/aosp-system-core

fs_mgr: moves common functions out of fs_mgr_verity.cpp
This commits moves some common functions out of fs_mgr_verity.cpp to be
reused by other verified boot flows. It includes:
 - Move common functions into fs_mgr.c
 - Move dm_ioctl related functions to a new file: fs_mgr_dm_ioctl.c

Bug: 30547241
Test: checks device can boot

Change-Id: Iaa0d8031efbaae12aa28f872f62d3fc3d3763b51

fs_mgr/Android.mk
@@ -18,6 +18,7 @@ LOCAL_CLANG := true
 LOCAL_SANITIZE := integer
 LOCAL_SRC_FILES:= \
     fs_mgr.c \
+    fs_mgr_dm_ioctl.c \
     fs_mgr_format.c \
     fs_mgr_fstab.c \
     fs_mgr_slotselect.c \

fs_mgr/fs_mgr_priv_dm_ioctl.h
@@ -1,0 +1,30 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __CORE_FS_MGR_PRIV_DM_IOCTL_H
+#define __CORE_FS_MGR_PRIV_DM_IOCTL_H
+
+__BEGIN_DECLS
+
+void verity_ioctl_init(struct dm_ioctl *io, const char *name, unsigned flags);
+int create_verity_device(struct dm_ioctl *io, char *name, int fd);
+int destroy_verity_device(struct dm_ioctl *io, char *name, int fd);
+int get_verity_device_name(struct dm_ioctl *io, char *name, int fd, char **dev_name);
+int resume_verity_table(struct dm_ioctl *io, char *name, int fd);
+
+__END_DECLS
+
+#endif /* __CORE_FS_MGR_PRIV_DM_IOCTL_H */

joacime/system_core

 - Move dm_ioctl related functions to a new file: fs_mgr_dm_ioctl.cpp
Bug: 31264231
Test: check device can boot with dm-verity

fs_mgr/Android.mk
@@ -18,6 +18,7 @@ LOCAL_CLANG := true
 LOCAL_SANITIZE := integer
 LOCAL_SRC_FILES:= \
     fs_mgr.c \
+    fs_mgr_dm_ioctl.cpp \
     fs_mgr_format.c \
     fs_mgr_fstab.c \
     fs_mgr_slotselect.c \

fs_mgr/fs_mgr_priv_dm_ioctl.h
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __CORE_FS_MGR_PRIV_DM_IOCTL_H
+#define __CORE_FS_MGR_PRIV_DM_IOCTL_H
+
+#include <linux/dm-ioctl.h>
+
+__BEGIN_DECLS
+
+void fs_mgr_verity_ioctl_init(struct dm_ioctl *io, const char *name, unsigned flags);
+int fs_mgr_create_verity_device(struct dm_ioctl *io, char *name, int fd);
+int fs_mgr_destroy_verity_device(struct dm_ioctl *io, char *name, int fd);
+int fs_mgr_get_verity_device_name(struct dm_ioctl *io, char *name, int fd, char **dev_name);
+int fs_mgr_resume_verity_table(struct dm_ioctl *io, char *name, int fd);
+
+__END_DECLS
+
+#endif /* __CORE_FS_MGR_PRIV_DM_IOCTL_H */