25fa2691db0149c57b005c9db8818f52aebbcc37
Tianjie <xunchang@google.com>
2020-06-08 21:12:49 -0700

    Update libsnapshot to handle partial update
    For partial update, the payload will not include all dynamic
    partitions on the device, update the logic libsnapshot to better
    handle such case.
    
    Bug: 157778739
    Test: run a partial OTA, unit tests pass
    
    Change-Id: I4339a81ed31161bab3ba9666c1d05fb8bf57dbf9

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b59efc45b5b5b1b5dedf6a83e10c9f252063da71045b528713f514040f17c3f5
-size 107169
+oid sha256:4c477d9eacfb809a9f43b3bb32f96ad1a5d26a90c96ddacf57a79aa8b9b4cc31
+size 107345
