1d171a456628501b82c6b5323921bfe6eb0fe285
Tianjie Xu <xunchang@google.com>
2020-10-03 07:27:00 +0000

    Revert "libsnapshot: Partially implement OpenSnapshotWriter."
    Revert "Link to libsnapshot_cow everywhere libsnapshot is linked."
    
    Revert submission 1433573-vab-libsnapshot-linkage
    
    Reason for revert: b/169981170, update crash for droidfooders.
    Reverted Changes:
    Ie75bba98c:Link to libsnapshot_cow where libsnapshot is linke...
    Ieedfadc55:libsnapshot: Partially implement OpenSnapshotWrite...
    I28a5d4a88:Link to libsnapshot_cow everywhere libsnapshot is ...
    
    Exempt-From-Owner-Approval: Revert to unblock dogfood
    Change-Id: I0677df77672aca9fd54d94e009ac0be7c88a1a9d

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26264f7817e66d6e5b43e793efacf01430b2f805683304eb3c9fb223a500c567
-size 111717
+oid sha256:737f4524d916c113350bcdc261b663fdf368e1ccabef9d931653bcdf2332ad84
+size 108839
