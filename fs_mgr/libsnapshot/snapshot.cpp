7816505f9b3466a6ecf08f8545c43690cabef42d
David Anderson <dvander@google.com>
2020-12-08 00:21:20 -0800

    init: Add an selinux transition for snapuserd.
    With compressed VAB updates, it is not possible to mount /system without
    first running snapuserd, which is the userspace component to the dm-user
    kernel module. This poses a problem because as soon as selinux
    enforcement is enabled, snapuserd (running in a kernel context) does not
    have access to read and decompress the underlying system partition.
    
    To account for this, we split SelinuxInitialize into multiple steps:
    
    First, sepolicy is read into an in-memory string.
    
    Second, the device-mapper tables for all snapshots are rebuilt. This
    flushes any pending reads and creates new dm-user devices. The original
    kernel-privileged snapuserd is then killed.
    
    Third, sepolicy is loaded from the in-memory string.
    
    Fourth, we re-launch snapuserd and connect it to the newly created
    dm-user devices. As part of this step we restorecon device-mapper
    devices and /dev/block/by-name/super, since the new snapuserd is in a
    limited context.
    
    Finally, we set enforcing mode.
    
    This sequence ensures that snapuserd has appropriate privileges with a
    minimal number of permissive audits.
    
    Bug: 173476209
    Test: full OTA with VABC applies and boots
    Change-Id: Ie4e0f5166b01c31a6f337afc26fc58b96217604e

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d15a8c7db815c52adcc46c65e69282c236ade3c38fdd55479aab4e498d2f669
-size 126206
+oid sha256:ae9a48afbae19ea99cd3298539ed2d855cb777a82d696b072abbddf6ea4c3f63
+size 127210
