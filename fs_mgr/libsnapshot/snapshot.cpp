0ba8fb54b1f37a6c65e6b419faa1ff1453bbbc3d
David Anderson <dvander@google.com>
2021-01-19 22:13:37 -0800

    libsnapshot: Use a two-phase merge for VABC.
    If a partition shrinks in an update, it must be merged before any other
    partitions. Otherwise, a copy operation may source from the tail of the
    shrunk partition, which could be overwritten by a merge operation in
    another partition.
    
    This patch adds a "MergePhase" indicator to the update status that is
    valid only when the state is MERGING. Partitions that shrink are merged
    first, and the phase will be FIRST_PHASE. Once ProcessUpdateState() has
    determined that all first-phase snapshots are merged, it will switch to
    SECOND_PHASE and remaining snapshots can start merging.
    
    Otherwise, there is no change to the merge algorithm. The phase split is
    an implementation detail and not exposed to update_engine.
    
    Bug: 177935716
    Test: vts_libsnapshot_test
    Change-Id: I06043f8e3b81bdecefb6a4b5944a97b7086eeb49

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e3b7009247f6423c2b035c72b4278f71323d19046d050d0bc135c3a8a4a517b
-size 126783
+oid sha256:308d34cc91331d32bcf8655b9944810b7e5bb6b25db49aeee5f3be3d1d8e478b
+size 130837
