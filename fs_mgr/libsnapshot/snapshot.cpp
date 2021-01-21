d820c8f7b0b69d14acfa64f1ffd07c55768b8c9e
David Anderson <dvander@google.com>
2021-01-21 14:15:54 -0800

    libsnapshot: Only enable compression when the corresponding bit is set in DeltaArchiveManifest.
    Test: vts_libsnapshot_test
    Change-Id: I43bab4a823d61c56596e1fd5b1522de08b9674f2

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:308d34cc91331d32bcf8655b9944810b7e5bb6b25db49aeee5f3be3d1d8e478b
-size 130837
+oid sha256:97178735e7126dcb5a54fcbe8926e618628d3e6eec80c194dfe38bd490f62deb
+size 131185
