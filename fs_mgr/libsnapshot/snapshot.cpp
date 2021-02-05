d2a0443ea0184f2f56dcce0e5112dbea1ea9076c
David Anderson <dvander@google.com>
2021-02-04 20:14:18 -0800

    Add support for compressed snapshot merges in fastboot.
    SnapshotManager::New() is now preferred in recovery. Previously we used
    NewForFirstStageMount(), which is technically incorrect as that enables
    code paths specifically for first-stage init.
    
    We also explicitly label the snapuserd context, since rootfs in recovery
    has unlabelled files.
    
    Finally, we add a timeout to internal calls to
    CreateSnapshotsAndLogicalPartitions. Without this, WaitForDevice() calls
    will terminate immediately, which breaks VABC given the more complex
    device stacking that is created.
    
    Bug: 168258606
    Test: fastboot snapshot-update merge
    Change-Id: I3a663b95c0b1eabaf14e6fde409c6902653c3c5e

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d3c192964569f5516cc648271baee496239c97d9330c141a2db132522cb4e70
-size 132468
+oid sha256:959c3d0c48e7dcabcdfdde0ae8d2b972e3cd4b3c5b8de29d85a391e770bb701d
+size 132575
