82cb914990a25efadb26f59e476cbd5f016c482f
David Anderson <dvander@google.com>
2021-02-25 19:57:22 -0800

    libsnapshot: Quell error spam during factory data resets.
    QuerySnapshotStatus assumes IsSnapshotDevice() would return true.
    
    Additionally, recovery does not have access to /dev/loop-control, which
    cannot be used by libfiemap anyway. Access it on-demand instead of
    preemptively.
    
    Bug: N/A
    Test: manual test
    Change-Id: I0f746870d7a8ec6d666f0bdd2fef3464b214928b

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a7ce8c71b489c2e7e97b595802e9a69cb937f6c6159d097e62329376b59ad98
-size 133542
+oid sha256:e8d720d57ce57880ee9114bd420238dfa62158960b0780c112ffaed128aba4f2
+size 133593
