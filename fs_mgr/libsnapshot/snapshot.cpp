c954bc5054442d9c40c9bb0a675fa37b03a7fcad
Akilesh Kailash <akailash@google.com>
2021-03-03 04:42:03 +0000

    libsnapshot:snapuserd: Multithreading support
    Add worker threads per partition to serve the IO request.
    
    Remove memset of buffer in IO path which was impacting
    4k IO performance.
    
    update_verifier performance:
    
    1: ~10-12 seconds with this change (both on full OTA and incremental
    OTA); ~70 seconds observed without this changeset
    
    2: ~8 seconds without the daemon once merge is completed
    and snapshot devices are removed.
    
    Bug: 181293939
    Test: update_verifier, full OTA, incremental OTA
    Signed-off-by: Akilesh Kailash <akailash@google.com>
    Change-Id: Id90887f3f4a664ee5d39433715d1c166acbd6c60

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91f60e2df2add5aa78e99bf87634a84d88dadba31fba8dc49e42eb092f403f03
+oid sha256:705ef495fd017ddc1325a999a44339c75a4fd81b8c7af5345116ba9c0b829be5
 size 135433
