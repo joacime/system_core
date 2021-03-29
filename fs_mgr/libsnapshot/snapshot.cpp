e9c3615255d386845032448b5e36924e26455c09
Akilesh Kailash <akailash@google.com>
2021-03-29 17:57:35 +0000

    libsnapshot:snapuserd: Add 2MB scratch space in COW file
    Add 2MB scratch space in the COW file. This is a preparation
    patch for read-ahead patch. This just add the buffer
    space right after the header. Bump up the version number
    in the header in order to distiguish between older and newer
    COW formats.
    
    No operation is done on this buffer with this patch-set.
    
    Scratch space option is disabled by default.
    
    Bug: 183863613
    Test: 1: Create Full OTA with the new COW format.
    2: Incremental OTA with older COW format.
    3: vts_libsnapshot_test
    
    Signed-off-by: Akilesh Kailash <akailash@google.com>
    Change-Id: I42a535a48ec22adb893dfe6f86a4f51650e1f88a

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b58fb6a25af26f51375857a14207372bf9adb0070db8d7361e3482c5b34ba35
-size 139637
+oid sha256:957efbc3de64712a0bce74152956ca836400bc637b18ff6d9435c306f78be237
+size 139919
