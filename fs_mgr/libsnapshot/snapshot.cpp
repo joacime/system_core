fb569333c0e1f973e117e75cc7b9135fae9ad5a0
Akilesh Kailash <akailash@google.com>
2021-04-13 22:34:14 +0000

    libsnapshot: Retrieve COW version from update engine manifest
    update_metadata.proto will have the COW version. Retrieve
    that from the manifest and compare it with the COW library.
    If the versioning doesn't match, disable VABC.
    
    The primary use case of this is during downgrade tests
    in pre-submit. Whenever we have a COW format changes,
    we may have to disable VABC for that specific transition
    build. At a high level, the flow of version check will be:
    
    1: Create a initial COW version of 1 in manifest (update_metadata.proto)
    2: The latest COW version of libsnapshot is 2
    3: libsnapshot will return VABC disabled
    4: Check-in the CL and changes to manifest
    5: Once the CL is baked in and the build is green, bump up the COW version to 2 in the manifest
    6: Next set of tests, since both versions match, libsnapshot will enable VABC
    7: Downgrade should be done to the build which was checked in at (5)
    
    Bug: 183863613
    Test: Apply OTA and verify if VABC is disabled if the versions don't
    match
    Signed-off-by: Akilesh Kailash <akailash@google.com>
    Change-Id: Id55f33a90bb31b417e72f4fbe370daf05a68f05a

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:957efbc3de64712a0bce74152956ca836400bc637b18ff6d9435c306f78be237
-size 139919
+oid sha256:7c1cd22ff578e160dfd50a4ffb09254c7ec9966c111b1e457c027c72cb425ff9
+size 140305
