70c58a15faa2f93735e9c3a3d8ce7a727b49a24d
David Anderson <dvander@google.com>
2021-07-16 20:02:11 -0700

    libsnapshot: Propagate merge phase across merge failures.
    If a merge fails we write a new snapshot status indicating that the merge
    failed. If this happens to occur during the second merge phase, we fail
    to propagate the phase counter to the new status. This means the merge
    is unlikely to make progress and succeed later.
    
    Bug: 213031779
    Bug: 213253413
    Bug: 193549218
    Ignore-AOSP-First: cherry-pick from AOSP
    Test: inject transient failure into CheckMergeConsistency, apply OTA,
          reboot and complete merge.
    Change-Id: I31fdae6bde48e3a71b6f3fcc663541257f7ebd8f
    Merged-In: I31fdae6bde48e3a71b6f3fcc663541257f7ebd8f

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9af74aeb39f4a50e3ed337698c8a5dba8d6f648dfcc56880958940784ed2608f
-size 147050
+oid sha256:b594548aad81c80745c7340b83e1e878aea5757db153bb79d543af570daab307
+size 147108
