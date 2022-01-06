e3dae2e40591d4b0ea0f9520417e512691b6d4d8
David Anderson <dvander@google.com>
2022-01-06 02:26:08 +0000

    libsnapshot: Fix CHECK failure during second phase merge
    This CHECK prevents a release build from resuming a two-phase merge if
    the merge initially failed in the first pass.
    
    Bug: 213031779
    Bug: 213253413
    Bug: 193549218
    Ignore-AOSP-First: cherry-pick from AOSP
    Test: vts_libsnapshot_test
    Test: update_engine_unittests
    Change-Id: I8bf00e3016546ef7039bb0b18eb977cc3dc1066a
    Merged-In: I8bf00e3016546ef7039bb0b18eb977cc3dc1066a

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b594548aad81c80745c7340b83e1e878aea5757db153bb79d543af570daab307
-size 147108
+oid sha256:df64fb03e4d5fb52357ff990ff9ba7aabd1e21e430e5b6d01dc3a362e7ff10f7
+size 147545
